133e0e7abb15d014b1d5011b791121a7bbe87683
alderisCode <ogotay76@gmail.com>
2025-01-13 16:08:59 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace GMLPretty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new DarkUI.Controls.DarkButton();
+            this.btnStart = new DarkUI.Controls.DarkButton();
+            this.darkStatusStrip1 = new DarkUI.Controls.DarkStatusStrip();
+            this.darkMenuStrip1 = new DarkUI.Controls.DarkMenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otwórzToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.darkStatusStrip1.SuspendLayout();
+            this.darkMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 27);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Padding = new System.Windows.Forms.Padding(5);
+            this.btnOpen.Size = new System.Drawing.Size(325, 27);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Otwórz plik GML/XML...";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(12, 79);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Padding = new System.Windows.Forms.Padding(5);
+            this.btnStart.Size = new System.Drawing.Size(325, 37);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Uczytelnij strukturę pliku";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // darkStatusStrip1
+            // 
+            this.darkStatusStrip1.AutoSize = false;
+            this.darkStatusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkStatusStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkStatusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.darkStatusStrip1.Location = new System.Drawing.Point(0, 128);
+            this.darkStatusStrip1.Name = "darkStatusStrip1";
+            this.darkStatusStrip1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 3);
+            this.darkStatusStrip1.Size = new System.Drawing.Size(349, 24);
+            this.darkStatusStrip1.SizingGrip = false;
+            this.darkStatusStrip1.TabIndex = 3;
+            this.darkStatusStrip1.Text = "darkStatusStrip1";
+            // 
+            // darkMenuStrip1
+            // 
+            this.darkMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkMenuStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.darkMenuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.darkMenuStrip1.Name = "darkMenuStrip1";
+            this.darkMenuStrip1.Padding = new System.Windows.Forms.Padding(3, 2, 0, 2);
+            this.darkMenuStrip1.Size = new System.Drawing.Size(349, 24);
+            this.darkMenuStrip1.TabIndex = 4;
+            this.darkMenuStrip1.Text = "darkMenuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otwórzToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.zamknijToolStripMenuItem});
+            this.plikToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // otwórzToolStripMenuItem
+            // 
+            this.otwórzToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.otwórzToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.otwórzToolStripMenuItem.Name = "otwórzToolStripMenuItem";
+            this.otwórzToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.otwórzToolStripMenuItem.Text = "Otwórz...";
+            this.otwórzToolStripMenuItem.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.toolStripMenuItem1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.toolStripMenuItem1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 1);
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.zamknijToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "gml";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Pliki GML|*.gml|Pliki XML|*.xml|Wszystkie pliki|*.*";
+            this.openFileDialog1.Title = "Otwórz plik GML/XML";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Pliki GML|*.gml|Pliki XML|*.xml|Wszystkie pliki|*.*";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI", 7.5F);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(0, 2, 0, 2);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(55, 12);
+            this.statusLabel.Text = "Otwórz plik";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 152);
+            this.Controls.Add(this.darkStatusStrip1);
+            this.Controls.Add(this.darkMenuStrip1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.darkMenuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GML Pretty";
+            this.darkStatusStrip1.ResumeLayout(false);
+            this.darkStatusStrip1.PerformLayout();
+            this.darkMenuStrip1.ResumeLayout(false);
+            this.darkMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DarkUI.Controls.DarkButton btnOpen;
+        private DarkUI.Controls.DarkButton btnStart;
+        private DarkUI.Controls.DarkStatusStrip darkStatusStrip1;
+        private DarkUI.Controls.DarkMenuStrip darkMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otwórzToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using DarkUI.Controls;
+using DarkUI.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace GMLPretty
+{
+    public partial class Form1 : DarkForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //openFileDialog1.FileName;
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) 
+            { 
+                statusLabel.Text = "Zapisuję do nowego pliku...";
+                Application.DoEvents();
+                MakeItPretty(openFileDialog1.FileName, saveFileDialog1.FileName);
+            }
+        }
+
+
+        void MakeItPretty(string fileNameSrc, string fileNameDest)
+        {
+            int nodeLevel = 0;
+            //GMLNode node = new GMLNode("");
+            int parentID = -1;
+            bool emptyNode = false;
+            bool isFeatureMember = false;
+            string line = "";
+            string elStart = "";
+            string elEnd = "";
+            string elParams = "";
+            string elValue = "";
+            XmlReader xmlReader = XmlReader.Create(fileNameSrc);
+            while (xmlReader.Read())
+            {
+                switch (xmlReader.NodeType)
+                {
+                    // znacznik początkowy
+                    case XmlNodeType.Element:
+                        elStart = xmlReader.Name;
+
+                        // poziom wcięcia...
+                        nodeLevel++;
+
+                        // atrybuty
+                        for (int attInd = 0; attInd < xmlReader.AttributeCount; attInd++)
+                        {
+                            xmlReader.MoveToAttribute(attInd);
+                            //Log(Lvl(nodeLevel) + "    - " + xmlReader.Name + " = " + xmlReader.Value + "\n", Color.Coral);
+                            // -- DB --
+                            //GMLAttribute attr = new GMLAttribute(node, xmlReader.Name, xmlReader.Value);
+                            //node.Attributes.Add(attr);
+                        }
+
+                        break;
+
+                    // Deklaracja
+                    case XmlNodeType.XmlDeclaration:
+                        line = xmlReader.Name;
+
+                        break;
+
+                    // tekst między znacznikami - value
+                    case XmlNodeType.Text:
+                        //Log(Lvl(nodeLevel) + "Text Node: " + xmlReader.Value + "\n");
+                        elValue = xmlReader.Value;
+                        break;
+
+                    // znacznik końcowy
+                    case XmlNodeType.EndElement:
+                        //node = DB_GetNode(node.ParentID);
+                        //parentID = node.ID;
+                        nodeLevel--;
+                        //Log(Lvl(nodeLevel) + "End Element " + xmlReader.Name + "\n" + Lvl(nodeLevel) + 
+                        //    "Wracamy do <" + node.Name + ">" , Color.OrangeRed);
+                        string t = Tabs(nodeLevel, 2);
+                        line = String.Concat(t, "<", elStart, " ", elParams, ">", elValue);
+                        break;
+
+                    // pusta linia
+                    case XmlNodeType.Whitespace:
+                        //Log(Lvl(nodeLevel) + ".\n", Color.Gray);
+                        break;
+
+                    default:
+                        //Log(Lvl(nodeLevel) + "Other node " + xmlReader.NodeType + " with value " + xmlReader.Value + "\n", Color.Cyan);
+                        break;
+
+                }
+
+            }
+
+
+
+        }
+
+
+        private string Tabs(int tabCount, int tabSize)
+        {
+            string tab = "";
+            string tabs = "";
+            for (int i = 0; i < tabSize; i++) { tab += " "; }
+            for (int i = 0; i < tabCount; i++) { tabs += tab; }
+            return tabs;
+        }
+
+    }
+}
